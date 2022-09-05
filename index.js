89db71425c9ac264671ed6410d0ab024f87468f7
anujanz <109773838+anujanz@users.noreply.github.com>
2022-09-05 11:17:47 -0400

    pokedex progress

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-nes (8 sloc)  195 Bytes
-
 function fetchPokemon() {
-  const requestURL = "https://pokeapi.co/api/v2/pokemon/pikachu";
+    let pokemonName = "Blank";
+    const PokemonNameMessage = "Pokemon Name:" + pokemonName;
+    document.getElementById("name").innerHTML = PokemonNameMessage
+  
 
-  fetch(requestURL)
-    .then((x) => x.json())
+   
+  //JS Promises
+  const requestURL = "https://pokeapi.co/api/v2/pokemon/pikachu";
+    fetch(requestURL)
+    .then((res) => res.json())
     .then((obj) => {
-      console.log(obj);
-    });
-}
+    console.log(obj);
+    const n = obj.name;
+    const PokemonNameMessage = "Pokemon Name:" + n;
+    document.getElementById("name").innerHTML = PokemonNameMessage
+    
+
+//Image
+    const imageUrl = obj.sprites.front_default;;
+    document.getElementById("image").src = imageUrl;
+
+// Move list
+let move1 = obj.moves[0].move.name;
+let move2 = obj.moves[1].move.name;
+let move3 = obj.moves[2].move.name;
+let move4 = obj.moves[3].move.name;
+document.getElementById("move-list").innerHTML =
+ `<li>${move1} </li> <li>${move2}  </li> <li>${move3} </li> <li>${move4}  </li>`;
+});
+     
+  }
+
+

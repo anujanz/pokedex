dcd45577657fc5c0b1d8364fe441cd29c4091045
anujanz <109773838+anujanz@users.noreply.github.com>
2022-09-26 10:01:57 -0400

    recent update

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
-function fetchPokemon() {
-    let pokemonName = "Blank";
-    const PokemonNameMessage = "Pokemon Name:" + pokemonName;
-    document.getElementById("name").innerHTML = PokemonNameMessage
-  
 
-   
-  //JS Promises
-  const requestURL = "https://pokeapi.co/api/v2/pokemon/pikachu";
-    fetch(requestURL)
-    .then((res) => res.json())
+function displayPokemon(obj){
+            //Name
+            console.log(obj);
+            const name = obj.name;
+            const PokemonNameMessage = "Pokemon Name:" + name;
+            document.getElementById("name").innerHTML = PokemonNameMessage;
+            
+        //Image
+            const imageUrl = obj.sprites.front_default;
+            document.getElementById("image").src = imageUrl;
+        
+        // Move list
+        const move1 = obj.moves[0].move.name;
+        const move2 = obj.moves[1].move.name;
+        const move3 = obj.moves[2].move.name;
+        const move4 = obj.moves[3].move.name;
+        document.getElementById("move-list").innerHTML =
+         `<li>${move1} </li> <li>${move2}</li> <li>${move3}</li> <li>${move4}</li>`;
+        }
+
+    function fetchPokemon(){
+        const requestURL = "https://pokeapi.co/api/v2/pokemon/";
+    const searchTerm = document.getElementById("search-box").value;
+    console.log(searchTerm);
+     fetch(requestURL+searchTerm)
+     .then((x) => x.json())
     .then((obj) => {
-    console.log(obj);
-    const n = obj.name;
-    const PokemonNameMessage = "Pokemon Name:" + n;
-    document.getElementById("name").innerHTML = PokemonNameMessage
-    
-
-//Image
-    const imageUrl = obj.sprites.front_default;;
-    document.getElementById("image").src = imageUrl;
-
-// Move list
-let move1 = obj.moves[0].move.name;
-let move2 = obj.moves[1].move.name;
-let move3 = obj.moves[2].move.name;
-let move4 = obj.moves[3].move.name;
-document.getElementById("move-list").innerHTML =
- `<li>${move1} </li> <li>${move2}  </li> <li>${move3} </li> <li>${move4}  </li>`;
-});
-     
-  }
-
-
+        displayPokemon(obj);
+        });
+    }
